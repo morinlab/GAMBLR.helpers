b0121b0fb3c0f78465495201b2e2839f70c14b3a
vladimirsouza <vladimirbcs@gmail.com>
2023-07-17 01:09:57 -0700

    Add more dependencies

---
+++ R/GAMBLR.helpers-package.R
@@ -0,0 +1,16 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom grDevices rainbow
+#' @importFrom stats end
+#' @importFrom stats quantile
+#' @importFrom stats start
+#' @importFrom utils head
+#' @importFrom utils read.csv
+#' @importFrom utils read.socket
+#' @importFrom utils tail
+#' @importFrom utils write.socket
+#' @importFrom utils write.table
+## usethis namespace: end
+NULL
