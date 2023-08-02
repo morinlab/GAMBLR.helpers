e0877b4cb62f9795571436666ca29594c4583d2c
Kdreval <k.dreval@gmail.com>
2023-08-02 16:47:11 -0700

    cleanup: add missing dependencies

--- R/GAMBLR.helpers-package.R
+++ R/GAMBLR.helpers-package.R
@@ -3,10 +3,14 @@
 
 ## usethis namespace: start
 #' @importFrom dplyr left_join
+#' @importFrom philentropy KL
 #' @importFrom reshape2 melt
 #' @importFrom stats end
 #' @importFrom stats quantile
 #' @importFrom stats start
+#' @importFrom stringr str_extract
+#' @importFrom stringr str_remove_all
+#' @importFrom tidyr unnest_auto
 #' @importFrom utils head
 #' @importFrom utils read.csv
 #' @importFrom utils read.socket
