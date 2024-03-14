03881d60c277a76432b0c5e9bd2e2b3b5196e000
Kdreval <k.dreval@gmail.com>
2024-03-14 13:56:36 -0700

    cleanup: drop reshape2 dependency

--- R/get_unmatched_normals.R
+++ R/get_unmatched_normals.R
@@ -8,11 +8,26 @@
 #'
 #' @return data frame
 #'
+#' @import dplyr tidyr tibble
 #' @export
 get_unmatched_normals = function(seq_type_filter){
-  a = check_config_value(config::get("unmatched_normal_ids"))
-  df = melt(a,value.name="normal_sample_id") %>%
-    rename(c("genome_build"="L3","seq_type"="L2","unix_group"="L1")) %>%
-    dplyr::filter(seq_type == seq_type_filter)
-  return(df)
+    a <- check_config_value(config::get("unmatched_normal_ids"))
+    df <- a %>%
+        as.data.frame(
+            check.names = FALSE
+        ) %>%
+        t %>%
+        as.data.frame() %>%
+        tibble::rownames_to_column("rownames") %>%
+        tidyr::separate(
+            rownames,
+            into = c("unix_group", "seq_type", "genome_build"),
+            sep = "\\."
+        ) %>%
+        dplyr::select(
+            "normal_sample_id" = "V1",
+            genome_build, seq_type, unix_group
+        ) %>%
+        dplyr::filter(seq_type == seq_type_filter)
+    return(df)
 }
